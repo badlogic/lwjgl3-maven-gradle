4bf9311664685c90d8c54f336b7ce8ccbe32bc00
Tue Ton <chirontt@gmail.com>
2020-12-28 23:26:23 -0500

    Updates to work with LWJGL 3.2.3 version
    - code in Lwjgl3Test.java updated to fix compile errors
    
    - SharedLibraryLoader class is not needed now, as LWJGL now loads the
    needed native libraries by itself at runtime from the classpath
    
    - Gradle/Maven build scripts updated to use minimal OpenGL set of LWJGL
    modules, for the 3 supported OS natives
    
    - the sample application Lwjgl3Test is fully working, and can be
    launched via the Gradle run command:
        gradlew run
    or via the Maven run command:
        mvn exec:java -Dexec.mainClass=Lwjgl3Test
    
    - Gradle wrapper updated to version 6.7.1
    
    - Gradle build script can produce a zip distribution, in
    build/distributions directory, via the 'gradlew distZip' command, and
    the resulting zip file should work in the 3 supported OS'es.

--- src/main/java/Lwjgl3Test.java
+++ src/main/java/Lwjgl3Test.java
@@ -1,13 +1,12 @@
-import org.lwjgl.Sys;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
- 
-import java.nio.ByteBuffer;
- 
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
  
 public class Lwjgl3Test {
  
@@ -19,7 +18,7 @@ public class Lwjgl3Test {
     private long window;
  
     public void run() {
-        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
  
         try {
             init();
@@ -27,21 +26,21 @@ public class Lwjgl3Test {
  
             // Release window and window callbacks
             glfwDestroyWindow(window);
-            keyCallback.release();
+            keyCallback.free();
         } finally {
             // Terminate GLFW and release the GLFWerrorfun
             glfwTerminate();
-            errorCallback.release();
+            errorCallback.free();
         }
     }
  
     private void init() {
         // Setup an error callback. The default implementation
         // will print the error message in System.err.
-        glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
  
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( glfwInit() != GL11.GL_TRUE )
+        if ( !glfwInit() )
             throw new IllegalStateException("Unable to initialize GLFW");
  
         // Configure our window
@@ -62,17 +61,17 @@ public class Lwjgl3Test {
             @Override
             public void invoke(long window, int key, int scancode, int action, int mods) {
                 if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+                    glfwSetWindowShouldClose(window, true); // We will detect this in our rendering loop
             }
         });
  
         // Get the resolution of the primary monitor
-        ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         // Center our window
         glfwSetWindowPos(
             window,
-            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
-            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+            (vidmode.width() - WIDTH) / 2,
+            (vidmode.height() - HEIGHT) / 2
         );
  
         // Make the OpenGL context current
@@ -85,20 +84,20 @@ public class Lwjgl3Test {
     }
  
     private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
+        // This line is critical for LWJGL's inter-operation with GLFW's
         // OpenGL context, or any context that is managed externally.
         // LWJGL detects the context that is current in the current thread,
         // creates the ContextCapabilities instance and makes the OpenGL
         // bindings available for use.
-        GLContext.createFromCurrent();
+        GL.createCapabilities();
  
         // Set the clear color
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
  
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        while ( !glfwWindowShouldClose(window) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the frame buffer
  
             glfwSwapBuffers(window); // swap the color buffers
  
@@ -107,9 +106,8 @@ public class Lwjgl3Test {
             glfwPollEvents();
         }
     }
- 
+
     public static void main(String[] args) {
-    	SharedLibraryLoader.load();
         new Lwjgl3Test().run();
     }
  
